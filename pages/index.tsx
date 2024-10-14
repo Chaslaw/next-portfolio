31b79ecf8b1e9392a1d54af129f08c13bec7005b
Caslav Lazic <caslavlazic@gmail.com>
2024-10-14 12:19:55 +0200

    Article back

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,8 @@ import About from 'components/Sections/About'
 import Experience from 'components/Sections/Experience'
 import FeaturedWorks from 'components/Sections/FeaturedWorks'
 import ScrollMore from 'components/Misc/ScrollMore'
-// import { Article } from 'types/article'
+import { SiLlvm } from 'react-icons/si'
+import { Article } from 'types/article'
 // These are on bottom sections so no need to render it instantly
 // const DevToArticles = dynamic(() => import('components/Sections/DevToArticles'))
 const GetInTouch = dynamic(() => import('components/Sections/GetInTouch'))
